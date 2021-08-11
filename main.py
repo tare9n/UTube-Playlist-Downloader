dd8c68d863449cfd3f0b7cebe1b1805f53b5675a
Taregh Naderi <taregh.n@gmail.com>
2021-08-11 19:57:43 +0430

    input folder name by user

--- main.py
+++ main.py
@@ -5,14 +5,15 @@ import youtube_dl
 import pathlib
 
 
-def check_url(url):
+def check_url(url, dir_name):
     if 'youtube.com/playlist?' in url:
-        get_inspect(url)
+        get_inspect(url, dir_name)
     elif 'youtube.com/watch?v' in url:
         with open('vid_links.txt', 'a', encoding='utf-8') as txt_file:
-            txt_file.write('\n' + url)
+            dir_name = 'Utube Playlist Downloader'
+            txt_file.write(f'\n{dir_name}\n' + url)
 
-def get_inspect(url):
+def get_inspect(url, dir_name):
     driver = webdriver.Chrome("chromedriver.exe")
     driver.get(url)
     sleep(3)
@@ -23,14 +24,14 @@ def get_inspect(url):
     html1 = driver.page_source
     html2 = driver.execute_script("return document.documentElement.innerHTML;")
     vid_ids = re.findall('(/watch\?v=.*index=\d*)\"', html2)
-    with open('vid_links.txt', 'w', encoding='utf-8') as my_file:
-        str_of_vid_ids = ''
+    with open('vid_links.txt', 'a', encoding='utf-8') as my_file:
+        str_of_vid_links = f'\n{dir_name}\n'
         for vid_id in vid_ids[2:-1]:
-            if vid_id in str_of_vid_ids:
+            if vid_id in str_of_vid_links:
                 continue
             else:
-                str_of_vid_ids += 'https://www.youtube.com' + vid_id + '\n'
-        my_file.write(str_of_vid_ids)
+                str_of_vid_links += 'https://www.youtube.com' + vid_id + '\n'
+        my_file.write(str_of_vid_links)
 
 def dl_video_list():
     current_dir = pathlib.Path().resolve()
@@ -40,25 +41,28 @@ def dl_video_list():
     download_list = link_list.copy()
     dir_name = 'Utube Playlist Downloader'
     for link in download_list:
-        if 'youtube.com/watch?v=' in link:
-            if 'index=' in link:
-                index = re.search('index=(\d*)', link).groups()[0]
-                ydl_opts = {
-                    'outtmpl': str(current_dir) + f'/{dir_name}/{index}- %(title)s.%(ext)s',
-                }
+        if link != '\n':
+            if link[:-2].isalnum() and 'youtube.com/watch?v=' not in link:
+                dir_name = link
+            elif 'youtube.com/watch?v=' in link:
+                if 'index=' in link:
+                    index = re.search('index=(\d*)', link).groups()[0]
+                    ydl_opts = {
+                        'outtmpl': str(current_dir) + f'/{dir_name}/{index}- %(title)s.%(ext)s',
+                    }
+                else:
+                    ydl_opts = {
+                        'outtmpl': str(current_dir) + f'/{dir_name}/%(title)s.%(ext)s',
+                    }
+                with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                    ydl.download([link])
+                link_list.pop(link_list.index(link))
+                txt_file = open('vid_links.txt', 'w', encoding='utf-8')
+                for link in link_list:
+                    txt_file.write(link)
+                txt_file.close()
             else:
-                ydl_opts = {
-                    'outtmpl': str(current_dir) + f'/{dir_name}/%(title)s.%(ext)s',
-                }
-            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-                ydl.download([link])
-            link_list.pop(link_list.index(link))
-            txt_file = open('vid_links.txt', 'w', encoding='utf-8')
-            for link in link_list:
-                txt_file.write(link)
-            txt_file.close()
-        else:
-            continue
+                continue
             
 def check_resume():
     txt_file = open('vid_links.txt', 'r+', encoding='utf-8')
@@ -72,14 +76,16 @@ def check_resume():
                 break
             elif answer == 'n':
                 url = input('Video or playlist url: ')
-                check_url(url)
+                dir_name = input('Enter a folder name to save videos:')
+                check_url(url, dir_name)
                 dl_video_list()
                 break
             else:
                 print('Invalid input')
     else:
         url = input('Video or playlist url: ')
-        check_url(url)
+        dir_name = input('Enter a folder name to save videos:')
+        check_url(url, dir_name)
         dl_video_list()
 
 def main():
